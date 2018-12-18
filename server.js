cb2bf59ce350c7a2d44c0951482c356d72b09037
R3tr0Mu4z <31041715+R3tr0Mu4z@users.noreply.github.com>
2018-12-19 04:42:36 +0600

    changed

--- server.js
+++ server.js
@@ -6,10 +6,18 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
 var aliexpress = require('./aliexpress.js');
 const PORT = process.env.PORT || 8080;
 app.get("/", (req, res) => {
     res.send("working");
+});
+app.get("/file", (req, res) => {
+    var file = req.query.name;
+    if (file.includes('.json')) {
+      res.setHeader('Content-Type', 'application/json');
+      res.sendFile( __dirname + "/files/" + file);
+    }
 });
 
 app.use('/aliexpress', aliexpress);
